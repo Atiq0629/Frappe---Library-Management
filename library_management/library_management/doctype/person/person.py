f6e5dc2568b4679688117dab976b6e41c5d89051
Atiq0629 <cross.016x@gmail.com>
2023-02-25 17:39:25 +0600

    Controllers

--- library_management/library_management/doctype/person/person.py
+++ library_management/library_management/doctype/person/person.py
@@ -1,23 +1,34 @@
-# Copyright (c) 2023, Atiq Rahman and contributors
+# Copyright (c) 2023, Frappe and contributors
 # For license information, please see license.txt
 
 import frappe
 from frappe.model.document import Document
 
 class Person(Document):
-    
-    def validate(self):
-        self.get_name()
-        
-    def get_full_name(self):
-        """Returns the full name of the person"""
-        return f"{self.first_name} {self.last_name}"
-    
-    def get_name(self):
-        doc = frappe.get_doc("Person", "Alex")
-        frappe.msgprint(doc.get_full_name())
- 
-    
-    
- 
- 
+	# def validate(self):
+	# 	if self.age < 18:
+	# 		frappe.throw("Person's age must be at least 18")
+		
+	# 	def after_insert(self):
+	# 		frappe.sendmail(recipients=[self.email], message="Thank you for registering")
+
+	def validate(self):
+		# self.create_document()
+		self.load_document()
+
+
+
+	# Create a document 
+	def create_document(self):	
+		doc = frappe.get_doc({
+			'doctype': 'Library Member',
+			'first_name': 'John',
+			'email_id':  'johnwick@gmail.com'
+		})
+		doc.insert()
+	
+	# Load a document
+	def load_document(self):
+		doc = frappe.get_doc('Library Member', 'MEM00001')
+		frappe.msgprint(doc.owner)
+		
